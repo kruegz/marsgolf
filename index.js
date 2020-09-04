05b69d318c6b94b0e37374c728cdc12330914ef3
Zach Krueger <krueger.zach@gmail.com>
2020-09-03 23:19:23 -0500

    Get example code

--- index.js
+++ index.js
@@ -5,78 +5,163 @@ window.addEventListener('DOMContentLoaded', function(){
     // load the 3D engine
     var engine = new BABYLON.Engine(canvas, true);
 
-    // createScene function that creates and return the scene
-    // var createScene = function(){
-    //     // create a basic BJS Scene object
-    //     var scene = new BABYLON.Scene(engine);
-
-    //     // create a FreeCamera, and set its position to (x:0, y:5, z:-10)
-    //     var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5,-10), scene);
-
-    //     // target the camera to scene origin
-    //     camera.setTarget(BABYLON.Vector3.Zero());
-
-    //     // attach the camera to the canvas
-    //     camera.attachControl(canvas, false);
-
-    //     // create a basic light, aiming 0,1,0 - meaning, to the sky
-    //     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0,1,0), scene);
-
-    //     // create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation 
-    //     var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene);
-
-    //     // move the sphere upward 1/2 of its height
-    //     sphere.position.y = 1;
-
-    //     // create a built-in "ground" shape;
-    //     var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene);
-
-    //     // return the created scene
-    //     return scene;
-    // }
-
     var createScene = function () {
         var scene = new BABYLON.Scene(engine);
+        scene.clearColor = BABYLON.Color3.Purple();
 
-        // Lights
-        var light0 = new BABYLON.DirectionalLight("Omni", new BABYLON.Vector3(-2, -5, 2), scene);
-        var light1 = new BABYLON.PointLight("Omni", new BABYLON.Vector3(2, -5, -2), scene);
-
-        // Need a free camera for collisions
-        var camera = new BABYLON.FreeCamera("FreeCamera", new BABYLON.Vector3(0, -8, -20), scene);
+        var camera = new BABYLON.ArcRotateCamera("Camera", 3 * Math.PI / 2, Math.PI / 2, 50, BABYLON.Vector3.Zero(), scene);
         camera.attachControl(canvas, true);
 
-        //Ground
-        var ground = BABYLON.Mesh.CreatePlane("ground", 10000.0, scene);
-        ground.material = new BABYLON.StandardMaterial("groundMat", scene);
-        ground.material.diffuseColor = new BABYLON.Color3(1, 1, 1);
-        ground.material.backFaceCulling = false;
-        ground.position = new BABYLON.Vector3(5, -10, -15);
-        ground.rotation = new BABYLON.Vector3(Math.PI / 2, 0, 0);
+        var light = new BABYLON.DirectionalLight("dir02", new BABYLON.Vector3(0.2, -1, 0), scene);
+        light.position = new BABYLON.Vector3(0, 80, 0);
 
-        //Simple crate
-        var box = BABYLON.Mesh.CreateBox("crate", 2, scene);
-        box.material = new BABYLON.StandardMaterial("Mat", scene);
-        box.material.diffuseTexture = new BABYLON.Texture("textures/crate.png", scene);
-        box.material.diffuseTexture.hasAlpha = true;
-        box.position = new BABYLON.Vector3(5, -9, -10);
+        // Shadows
+        var shadowGenerator = new BABYLON.ShadowGenerator(2048, light);
 
-        //Set gravity for the scene (G force like, on Y-axis)
-        scene.gravity = new BABYLON.Vector3(0, -0.9, 0);
+        // Box
+        var box = BABYLON.MeshBuilder.CreateBox("Box", {size:4}, scene);
+        box.position = new BABYLON.Vector3(-2, 4, 0);
+        var materialWood = new BABYLON.StandardMaterial("wood", scene);
+        materialWood.diffuseTexture = new BABYLON.Texture("textures/crate.png", scene);
+        materialWood.emissiveColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+        box.material = materialWood;
 
-        // Enable Collisions
-        scene.collisionsEnabled = true;
+        shadowGenerator.addShadowCaster(box);
 
-        //Then apply collisions and gravity to the active camera
-        camera.checkCollisions = true;
-        camera.applyGravity = true;
 
-        //Set the ellipsoid around the camera (e.g. your player's size)
-        camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
+        // Ground (using a box not a plane)
+        var ground = BABYLON.MeshBuilder.CreateBox("Ground", {width: 100, height: 1, depth: 100}, scene);
+        ground.position.y = -5.0;
 
-        //finally, say which mesh will be collisionable
-        ground.checkCollisions = true;
-        box.checkCollisions = true;
+        var groundMat = new BABYLON.StandardMaterial("groundMat", scene);
+        groundMat.diffuseColor = new BABYLON.Color3(0.5, 0.5, 0.5);
+        groundMat.emissiveColor = new BABYLON.Color3(0.2, 0.2, 0.2);
+        groundMat.backFaceCulling = false;
+        ground.material = groundMat;
+        ground.receiveShadows = true;
+
+        // Physics
+        scene.enablePhysics(null, new BABYLON.CannonJSPlugin());
+        //scene.enablePhysics(null, new BABYLON.OimoJSPlugin());
+        //scene.enablePhysics(null, new BABYLON.AmmoJSPlugin());
+
+        // Add Imposters
+        box.physicsImpostor = new BABYLON.PhysicsImpostor(box, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 2, friction: 0.0, restitution: 0.3 }, scene);
+        ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, friction: 0.0, restitution: 0.7 }, scene);
+       
+        //Impulse Settings
+        var impulseDirection = new BABYLON.Vector3(0, 1, 0);
+        var impulseMagnitude = 5;
+        var contactLocalRefPoint = BABYLON.Vector3.Zero();
+
+        var Pulse = function() {
+            box.physicsImpostor.applyImpulse(impulseDirection.scale(impulseMagnitude), box.getAbsolutePosition().add(contactLocalRefPoint));
+        }
+        
+        //GUI
+        
+        var changeX = function(value) {
+            impulseDirection.x = value;
+        }
+
+        var changeY = function(value) {
+            impulseDirection.y = value;
+        }
+        
+        var changeZ = function(value) {
+            impulseDirection.z = value;
+        }
+
+        var magnitude = function(value) {
+            impulseMagnitude = value;
+        }
+
+        var contactX = function(value) {
+            contactLocalRefPoint.x = value;
+        }
+
+        var contactY = function(value) {
+            contactLocalRefPoint.y = value;
+        }
+
+        var frictionBox = function(value) {
+            box.physicsImpostor.friction = value;
+        }
+
+        var frictionGround = function(value) {
+            ground.physicsImpostor.friction = value;
+        }
+
+        var contactZ = function(value) {
+            contactLocalRefPoint.z = value;
+        }
+        
+        var displayDValue = function(value) {
+            return Math.floor(value * 100) / 100;
+        }
+
+        var displayMValue = function(value) {
+            return Math.floor(value);
+        }
+
+        var displayFValue = function(value) {
+            return Math.floor(value * 10) / 10;
+        }
+        
+        var advancedTexture = BABYLON.GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI");
+
+        var selectBox = new BABYLON.GUI.SelectionPanel("spi");
+        selectBox.width = 0.25;
+        selectBox.height = 0.8;
+        selectBox.background = "#1388AF";
+        selectBox.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_LEFT;
+        selectBox.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+         
+        advancedTexture.addControl(selectBox);
+
+        var impulseMGroup = new BABYLON.GUI.SliderGroup("Impulse Magnitude", "S");
+        impulseMGroup.addSlider("Value", magnitude, "units", 0, 100, 5, displayMValue);
+        
+        var impulseDGroup = new BABYLON.GUI.SliderGroup("Impulse Direction", "S");
+        impulseDGroup.addSlider("X", changeX, "units", -1, 1, 0, displayDValue);
+        impulseDGroup.addSlider("Y", changeY, "units", -1, 1, 1, displayDValue);
+        impulseDGroup.addSlider("Z", changeZ, "units", -1, 1, 0, displayDValue);
+
+        var contactGroup = new BABYLON.GUI.SliderGroup("Contact Position", "S");
+        contactGroup.addSlider("X", contactX, "units", -2, 2, 0, displayDValue);
+        contactGroup.addSlider("Y", contactY, "units", -2, 2, 0, displayDValue);
+        contactGroup.addSlider("Z", contactZ, "units", -2, 2, 0, displayDValue); 
+
+        selectBox.addGroup(impulseMGroup);
+        selectBox.addGroup(impulseDGroup);
+        selectBox.addGroup(contactGroup);
+
+        var button = BABYLON.GUI.Button.CreateSimpleButton("but", "Swing");
+        button.width = 0.2;
+        button.height = "40px";
+        button.color = "white";
+        button.background = "#1388AF";
+        button.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_CENTER;
+        button.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+        button.top = "-10px";
+
+        button.onPointerClickObservable.add(Pulse)
+        advancedTexture.addControl(button);var selectBox = new BABYLON.GUI.SelectionPanel("spi");
+        
+        var selectFrictionBox = new BABYLON.GUI.SelectionPanel("spi");
+        selectFrictionBox.width = 0.25;
+        selectFrictionBox.height = 0.25;
+        selectFrictionBox.background = "#1388AF";
+        selectFrictionBox.horizontalAlignment = BABYLON.GUI.Control.HORIZONTAL_ALIGNMENT_RIGHT;
+        selectFrictionBox.verticalAlignment = BABYLON.GUI.Control.VERTICAL_ALIGNMENT_BOTTOM;
+
+        advancedTexture.addControl(selectFrictionBox);
+
+        var frictionGroup = new BABYLON.GUI.SliderGroup("Friction Values", "S");
+        frictionGroup.addSlider("Box", frictionBox, "units", 0, 10, 0, displayFValue);
+        frictionGroup.addSlider("Ground", frictionGround, "units", 0, 10, 0, displayFValue);
+
+        selectFrictionBox.addGroup(frictionGroup);
 
         return scene;
     }
